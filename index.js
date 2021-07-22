ee106650a02dee19c773ee419d2d5179cb1f19ae
Phing An Hsiao <phinganhsiao@gmail.com>
2021-07-22 14:33:12 -0400

    commit tree 792fecd949f373dd10e1cbd1f5726968e892c54a

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+// function superbowlWin(record) {
+//    if (find(result === "W"))
+//    return superbowlWin;
+// // }
+// function superbowlWin(record){
+//     let theYear = record.find(record.result);
+//     if (theYear === "W") {
+//     return theYear;
+// }}
+// console.log(theYear);
 
+// function superbowlWin(inYear) {
+//     return inYear.year === "W";
+// }
+// console.log(record.find(superbowlWin));
+
+const superbowlWin = record.find( ({ year }) => year === 'W' );
+console.log(superbowlWin)
+// = record.find((record) => record.result === "W");
+// return superbowlWin.year;
+
+// function superbowlWin(record) {
+//     let winYear = record.find(result => result === "W");
+//     return winYear;
+
+// function superbowlWin(myArray) {
+//     let yearWin = myArray.find(element => element.result === "W")
+//       return yearWin.year
+//   }
+//   superbowlWin(record)
